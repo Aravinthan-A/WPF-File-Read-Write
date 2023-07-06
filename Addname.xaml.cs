1a24bf3da00cb8f01ab2255648da9d018701c88f
Aravinthan <134296054+Aravinthan-A@users.noreply.github.com>
2023-07-06 12:11:13 +0530

    Add files via upload

---
+++ Addname.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Windows;
+using System.IO;
+using System;
+
+namespace String_Function
+{
+    /// <summary>
+    /// Interaction logic for Addname.xaml
+    /// </summary>
+    public partial class Addname : Window
+    {
+        ArrayList oarray;
+
+
+        public Addname()
+        {
+            InitializeComponent();
+           
+           
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (oarray == null)
+            {
+                oarray = new ArrayList();
+            }
+            oarray.Add(txtname.Text);
+            int count = oarray.Capacity;
+            cmbdata.ItemsSource = null;
+            cmbdata.ItemsSource = oarray;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string val = "";
+            int count = cmbdata.Items.Count;
+            for (int i = 0; i < count; i++)
+            {
+               val+= cmbdata.Items[i].ToString()+"|";
+            }
+            File.WriteAllText("C:\\Users\\welcome\\Documents\\" + txtname.Text + ".txt",val);
+        }
+
+       
+    }
+}
